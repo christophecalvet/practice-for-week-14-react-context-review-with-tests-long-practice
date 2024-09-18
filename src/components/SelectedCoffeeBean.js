86aadefd2a1fc9ae2c4fcbf0007981586641a1e2
Christophe Calvet <onlyatest@mytest.com>
2024-09-18 15:42:13 +0200

    seems to work also when changing intial default coffeebean

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,7 +1,12 @@
+
+import {CoffeeContext} from "../context/CoffeeContext";
+import { useContext } from "react";
+
 const SelectedCoffeeBean = () => {
+  const {coffeeBean} = useContext(CoffeeContext);
   return (
     <div className="selected-coffee">
-      <h2>Current Selection: </h2>
+      <h2>Current Selection: {coffeeBean.name}</h2>
     </div>
   );
 }
--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,14 +1,23 @@
+import { CoffeeContext } from "../context/CoffeeContext";
+import { useContext } from "react";
+
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+
+  const {coffeeBean, setCoffeeBeanId} = useContext(CoffeeContext);
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={(e) => setCoffeeBeanId(e.target.value)}
       >
         {coffeeBeans.map(bean => (
           <option
             key={bean.id}
             value={bean.id}
+            selected = {bean.id === coffeeBean.id? true: false}
           >
             {bean.name}
           </option>
